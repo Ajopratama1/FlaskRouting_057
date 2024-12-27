1a9e977a3169543e09f2a02cbbe21cdaff5b55b0
Ajopratama1 <161304006+Ajopratama1@users.noreply.github.com>
2024-12-27 20:53:25 +0700

    Update app.py

--- FlaskRouting_AhmadAjoPratama_057/app.py
+++ FlaskRouting_AhmadAjoPratama_057/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, url_for, request, render_template
 
-app = Flask(__name__)  
+app = Flask(__name__)   # Pake __name__
 
 @app.route('/')
 def home():
-    return render_template('index.html')  
+    return render_template('index.html')  # make file index.html di folder templates
 
 @app.route('/success/<name>')
 def success(name):
@@ -16,7 +16,7 @@ def login():
         user = request.form['nm']
         return redirect(url_for('success', name=user))
     else:
-        return redirect(url_for('home'))  
+        return redirect(url_for('home'))  # ngarahin kembali ke form jika gada data
 
 if __name__ == '__main__':
     app.run(debug=True)
